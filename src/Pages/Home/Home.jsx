855a154d1ee935b55f96d6d01eae28e0413330e1
Zaid Hassan <zaidhassan251@gmail.com>
2025-04-12 23:25:43 +0530

    Update Home page

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from "react";
+import { motion } from "motion/react";
 
 function Home() {
   return (
-    <div>Home</div>
-  )
+    <div className="">
+      <div className="flex flex-col justify-center items-center space-y-4 h-screen">
+        <motion.div
+          className=""
+          initial={{ y: "10vh", opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{ type: "spring", stiffness: 120 }}
+        >
+          <span className="text-3xl md:text-4xl text-center">Hello,</span> {""}
+          <br className="md:hidden"/>
+          <span className="text-xl md:text-2xl text-center">I am </span>
+          <span className="text-4xl md:text-5xl font-bold text-purple-700 text-center">
+            Zaid Hassan
+          </span>
+        </motion.div>
+
+        <motion.h1
+          className="text-lg w-[80%] md:w-full md:text-xl text-center"
+          initial={{ y: "10vh", opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{ type: "spring", stiffness: 120, delay: 0.4 }}
+        >
+          I'm Front-End Developer and a Game Developer
+        </motion.h1>
+      </div>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
--- src/components/Navbar/DesktopNavbar.jsx
+++ src/components/Navbar/DesktopNavbar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function DesktopNavbar() {
   return (
-    <div>DesktopNavbar</div>
+    <div className=''>DesktopNavbar</div>
   )
 }
 
