31fb0841e3d42c9c5d5225fa9985091980b78ab1
Zaid Hassan <zaidhassan251@gmail.com>
2025-04-12 23:29:22 +0530

    fix nav

--- src/components/Navbar/DesktopNavbar.jsx
+++ src/components/Navbar/DesktopNavbar.jsx
@@ -20,33 +20,6 @@ export default function DesktopNavbar() {
   }, [selectedTab]);
 
   return (
-<<<<<<< HEAD
-    <nav className="relative flex h-14 rounded-lg justify-center gap-8 mx-auto backdrop-blur-lg p-2 shadow-lg">
-      <div className="relative flex gap-8">
-        {tabs.map((tab) => (
-          <NavLink
-            key={tab.label}
-            to={tab.path}
-            onClick={() => dispatch(setSelectedTab(tab.label.toLowerCase()))}
-            className="relative px-5 py-2 text-black text-lg font-medium transition-all duration-300"
-          >
-            {tab.label}
-=======
     <div className=''>DesktopNavbar</div>
   )
-}
->>>>>>> three
-
-            {selectedTab === tab.label.toLowerCase() && (
-              <motion.div
-                layoutId="active-tab"
-                className="absolute left-0 right-0 -bottom-1 h-1 bg-blue-500 rounded-full"
-                transition={{ type: "spring", stiffness: 300, damping: 25 }}
-              />
-            )}
-          </NavLink>
-        ))}
-      </div>
-    </nav>
-  );
 }
