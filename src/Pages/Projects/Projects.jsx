9b2c34c91bfdf8b0f3bcce6e3bbb6621ba8d4d14
Zaid Hassan <zaidhassan251@gmail.com>
2025-06-06 04:25:23 +0530

    Under maintainance

--- src/Pages/Projects/Projects.jsx
+++ src/Pages/Projects/Projects.jsx
@@ -5,7 +5,7 @@ import TransitionDiv from "../../components/TransitionDiv/TransitionDiv";
 function Projects() {
   return (
     <motion.div
-      key="about"
+      key="project"
       className="flex flex-col justify-start items-start px-6 py-12 min-h-screen bg-gruv-soft-background"
     >
       <motion.h1
@@ -17,7 +17,16 @@ function Projects() {
       >
         Projects
       </motion.h1>
-      <TransitionDiv />
+  <motion.h1
+        className="text-xl md:text-2xl font-extrabold mb-6 text-gruv-soft-heading font-jetbrains-mono mx-auto"
+        initial={{ y: "10vh", opacity: 0, scale: 0.8 }}
+        animate={{ y: 0, opacity: 1, scale: 1 }}
+        exit={{ y: "-10vh", opacity: 0, scale: 0.8 }}
+        transition={{ ease: "easeInOut", delay: 0.6 }}
+      >
+	  Under Maintainance
+	  </motion.h1>
+<TransitionDiv />
     </motion.div>
   );
 }
